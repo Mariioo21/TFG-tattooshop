9f65f93993bc789571ecce89071e07ba6a9634f1
Mariioo21 <espasandinhernandez@gmail.com>
2025-09-28 20:35:20 +0200

    Subida del proyecto spring

---
+++ tattoshop-backend/tattoshop-backend/src/main/java/com/tattooshop/TattooshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tattooshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooshopBackendApplication.class, args);
+	}
+
+}
---
+++ tattoshop-backend/tattoshop-backend/src/test/java/com/tattooshop/TattooshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tattooshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
